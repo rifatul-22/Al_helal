1bdff1664a54e51119c70fe74d5e18fc68636c95
rifatul-22 <rifatulislam455@gmail.com>
2023-05-30 17:54:55 +0600

    v 1.0.3

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    // $('.insp_slider').slick({
+    //     slidesToShow: 2,
+    //     arrows: true,
+    //     prevArrow:$('.inspiration_slick_icon_btn.prev'),
+    //     nextArrow:$('.inspiration_slick_icon_btn.next')
+    // });
+});
+
+new VenoBox({
+    spinner: 'plane',
+});
